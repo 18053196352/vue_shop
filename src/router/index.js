ce9c34ea9620e904d01450a66559cb9507d9c12f
generous <1641106882@qq.com>
2021-03-09 15:15:50 +0800

    order

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,9 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
+
+
 
 Vue.use(Router)
 const router = new Router({
@@ -21,18 +24,17 @@ const router = new Router({
       component: Home,
       redirect: '/welcome',
       children: [
-        { path: '/welcome', component: Welcome }, { path: '/users', component: Users }, {
-          path: '/rights', component: Rights
-        },
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
         { path: "/params", component: Params },
         { path: "/goods", component: List },
         { path: "/goods/add", component: Add },
-
+        { path: '/orders', component: Order },
       ]
     },
-
   ]
 })
 // 挂载路由导航守卫
